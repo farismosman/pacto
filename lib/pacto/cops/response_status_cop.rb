c1f9a25fa012160ebe75030cd2678d9de0750e36
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2015-06-24 17:33:03 +0200

    allowing multiple statuses to be specified

--- lib/pacto/cops/response_status_cop.rb
+++ lib/pacto/cops/response_status_cop.rb
@@ -4,10 +4,19 @@ module Pacto
       def self.investigate(_request, response, contract)
         expected_status = contract.response.status
         actual_status = response.status
+
         errors = []
-        if expected_status != actual_status
-          errors << "Invalid status: expected #{expected_status} but got #{actual_status}"
+
+        if expected_status.is_a? Array
+          if !expected_status.include? actual_status
+            errors << "Invalid status: expected one of #{expected_status} but got #{actual_status}"
+          end
+        else
+          if expected_status != actual_status
+            errors << "Invalid status: expected #{expected_status} but got #{actual_status}"
+          end
         end
+
         errors
       end
     end
